4a2678bb4acb7794bce53de33c8ea3855a351925
AshSimmonds <ashsimmonds@gmail.com>
2023-10-16 13:25:08 +1000

    setup github admin

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,11 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'github',
+    repo: {
+        owner: 'AshSimmonds',
+        name: 'pip'
+    }
   },
   collections: {
     posts: collection({
